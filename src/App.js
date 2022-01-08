554aa940f8215bf8528b476527d1807dbe439556
antalex <antonachealexandrulucian@gmail.com>
2022-01-08 19:18:50 +0200

    created the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='bg-gray-800 h-screen'>
+      <h1 className='text-yellow-300 text-3xl font-black text-center pt-36'>IT IS RUNNING</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
